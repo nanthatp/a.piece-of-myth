2524434b671260a75a57e502ad8c1b27afff876b
WanwisaTT <wanisa7758@gmail.com>
2023-06-29 15:14:23 +0700

    long du si

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+//rest object
+const app = express();
+
+//rest api
+app.get('/',(req,res) => {
+    res.send({
+        message:'Welcome to a piece of MYTH app'
+    });
+});
+
+//POST
+const PORT = 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`.bgCyan.white);
+});
