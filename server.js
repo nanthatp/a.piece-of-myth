81f787dc051d83f63f2f742d5470d0ba7be9d306
WanwisaTT <wanisa7758@gmail.com>
2023-06-29 18:18:38 +0700

    long du kaaa

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
-const express = require('express');
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan  from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
 
 //rest object
 const app = express();
 
+//middelwares
+app.use(express.json())
+app.use(morgan(`dev`))
+
+//routes
+app.use('/api/v1/auth', authRoute)
+
 //rest api
 app.get('/',(req,res) => {
-    res.send({
-        message:'Welcome to a piece of MYTH app'
-    });
+    res.send("<h1>Welcome to a piece of MYTH app<h1>");
 });
 
 //POST
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
+    );
 });
