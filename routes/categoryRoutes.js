be9139702b470b4e7617c3f1ca0e15cd5502ad37
nanthatp <nanthatchaporn245@gmail.com>
2023-07-08 13:52:25 +0700

    update category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,17 +1,41 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
-import { createCategoryController } from '../controllers/categoryController.js';
+import { categoryController, createCategoryController, updateCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
 
 //routes
-// create category
+
+//create category
 router.post(
     "/create-category",
     requireSignIn,
     isAdmin,
     createCategoryController
 );
+
+//update category 
+router.put(
+    "/update-category/:id",
+    requireSignIn,
+    isAdmin,
+    updateCategoryController
+);
+
+//getAll category
+router.get('/get-category', categoryController)
+
+//single category
+router.get('/single-category', categoryController)
+
+//delete category 
+router.delete(
+    "/delete-category/:id",
+    requireSignIn,
+    isAdmin,
+    deleteCategoryCOntroller
+);
+
 
 export default router
