6c65f00ddc4d0b63afe0bee5101ef3dbacb3e341
WanwisaTT <wanisa7758@gmail.com>
2023-07-29 15:30:03 +0700

    update profile dai leaw ka

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerController, testController } from '../controllers/authController.js'
+import { registerController, testController, updateProfileController } from '../controllers/authController.js'
 import { loginController } from "../controllers/authController.js";
 import { forgotPasswordController } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
@@ -29,3 +29,6 @@ router.get("/user-auth", requireSignIn, (req, res) => {
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
     res.status(200).send({ ok: true });
 });
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
